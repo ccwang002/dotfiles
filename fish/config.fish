5fb9eb051504e81f4adf1df6d32877d1870d4c3c
Liang Bo Wang <ccwang002@gmail.com>
2014-03-08 11:08:18 +0800

    fix if begin clause

--- fish/config.fish
+++ fish/config.fish
@@ -14,7 +14,7 @@ set -x PATH $HOME/.cabal/bin /usr/local/bin $PATH
 # For pyenv
 set -x PYENV_ROOT /usr/local/opt/pyenv
 set -x PATH $PYENV_ROOT"/bin" $PATH
-if begin status --is-interactive; and type -f pyenv > /dev/null; end
+if begin; status --is-interactive; and type -f pyenv > /dev/null; end
     . (pyenv init -|psub)
 end
 
