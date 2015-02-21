159bdcdf0daca1b9c6688f5b8468b59b44323885
Liang Bo Wang <ccwang002@gmail.com>
2015-02-21 17:24:20 +0800

    properly use oh-my-fish setting

--- fish/config.fish
+++ fish/config.fish
@@ -1,21 +1,18 @@
 # Path to your oh-my-fish.
 set fish_path $HOME/.oh-my-fish
 
-# Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Example format: set fish_plugins autojump bundler
-
 # Path to your custom folder (default path is $FISH/custom)
 set fish_custom $HOME/dotfiles/fish
 
-# Add aliases
-set fish_function_path $HOME/dotfiles/fish/functions $fish_function_path
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
+# Custom plugins may be added to #FISH/custom/plugins
+# Example format: set fish_plugins autojump bundler
 
 # Theme
 set fish_theme bobthefish
+
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
 
 # LOCALE
 set -x LANG en_US.UTF-8
@@ -52,7 +49,6 @@ function fish_right_prompt
         end
     set_color normal
 end
-
 
 # Init pyenv
 if begin; status --is-interactive; and type -f pyenv > /dev/null; end
