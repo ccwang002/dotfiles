078135f8985a38adde365a2b651a8a5deb474acb
Liang Bo Wang <ccwang002@gmail.com>
2015-03-27 16:38:44 +0800

    add dotfile setup

---
+++ dotfile_setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""\
+Dotfiles setup script.
+
+Update the symlink_setting string as the following format:
+
+Source  Destination
+------  -----------
+
+"""
+from pathlib import Path
+
