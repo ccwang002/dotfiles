afbda9ca83a7a50586d899ffc88b9a4aea6af0e8
Liang Bo Wang <ccwang002@gmail.com>
2015-05-25 21:58:51 +0800

    fix vmmosh

--- fish/functions/vmmosh.fish
+++ fish/functions/vmmosh.fish
@@ -1,3 +1,3 @@
 function vmmosh --description 'mosh without no key nor host check, pass certfile as 1st argument'
-    mosh --ssh="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $argv[2..-1]" $argv[1]
+    mosh --ssh="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" $argv[1]
 end
