e37f686f9ffa8918de3a66ab98ad92964d501090
Liang-Bo Wang <me@liang2.tw>
2017-01-21 16:16:31 -0600

    Use original bobthefish theme; drop powerline font

--- omf/before.init.fish
+++ omf/before.init.fish
@@ -5,9 +5,28 @@
 # set fish_function_path $HOME/dotfiles/fish/functions $fish_function_path
 
 # Theme settings
-set -g theme_display_virtualenv yes
+set -g default_user "liang"
+set -g fish_prompt_pwd_dir_length 0
+set -g theme_color_scheme "dark"
+set -g theme_date_format "+%Y-%m-%d %H:%M"
+set -g theme_display_cmd_duration yes
+set -g theme_display_date yes
+set -g theme_display_docker_machine no
+set -g theme_display_git yes
 set -g theme_display_git_ahead_verbose yes
+set -g theme_display_git_untracked yes
 set -g theme_display_hg no
-set -g theme_display_user no
 set -g theme_display_ruby no
-set -g default_user liang
+set -g theme_display_user no
+set -g theme_display_vagrant no
+set -g theme_display_vi yes
+set -g theme_display_virtualenv yes
+set -g theme_git_worktree_support no
+set -g theme_nerd_fonts no
+set -g theme_powerline_fonts no
+set -g theme_project_dir_length 1
+set -g theme_show_exit_status yes
+set -g theme_title_display_path yes
+set -g theme_title_display_process yes
+set -g theme_title_display_user yes
+set -g theme_title_use_abbreviated_path no
