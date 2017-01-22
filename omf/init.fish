52c2d8b6fbfe9cc29c0305a8a25f457e6694ed0b
Liang-Bo Wang <me@liang2.tw>
2017-01-22 16:00:47 -0600

    init.fish cleanup
    - Only run the settings when encounter the login shell
    - Disable pyenv settings
    - Use miniconda directly
    - Add conda fish script if miniconda is installed
    - Disable python venv changing the prompt

--- omf/init.fish
+++ omf/init.fish
@@ -1,36 +1,36 @@
-# LOCALE
-set -x LANG en_US.UTF-8
-set -x LC_CTYPE en_US.UTF-8
+if status --is-login
+    # LOCALE
+    set -gx LANG en_US.UTF-8
+    set -gx LC_CTYPE en_US.UTF-8
 
-# Update PATH for homebrew
-test -d "$HOME/.cabal/bin"; and set -x PATH $HOME/.cabal/bin $PATH
-set -x PATH /usr/local/sbin /usr/local/bin $PATH
+    # Update PATH for homebrew
+    test -d "$HOME/.cabal/bin"; and set -x PATH $HOME/.cabal/bin $PATH
+    set -x PATH /usr/local/sbin /usr/local/bin $PATH
 
-# 256 color term
-# See Arch wiki: https://wiki.archlinux.org/index.php/Home_and_End_keys_not_working
-# Usually is a bad idea to set $TERM in config instead let terminal to decide
-# set -x TERM "xterm-256color"
+    # For rustup.rs
+    # Ref: https://www.rustup.rs/
+    test -d "$HOME/.cargo/bin"; and set -x PATH $HOME/.cargo/bin $PATH
 
-# For rustup.rs
-# Ref: https://www.rustup.rs/
-test -d "$HOME/.cargo/bin"; and set -x PATH $HOME/.cargo/bin $PATH
+    # For pyenv (init at the end of config)
+    # set -x PYENV_ROOT $HOME/.pyenv
+    # set -x PATH $PYENV_ROOT/bin $PATH
 
-# For pyenv (init at the end of config)
-set -x PYENV_ROOT $HOME/.pyenv
-# set -x PATH $PYENV_ROOT/bin $PATH
+    # Init pyenv
+    # if begin; status --is-interactive; and which pyenv > /dev/null; end
+    #     source (pyenv init - | psub)
+    # end
 
-# For virtualfish
-# newer virtualfish after `pip install virtualfish`
-#   eval (python -m virtualfish)
+    # Python environment
+    # Disable Python venv prompt changing
+    set -gx VIRTUAL_ENV_DISABLE_PROMPT "yes"
 
-# For pip
-set -x PIP_USE_WHEEL "true"
+    # conda http://conda.pydata.org/miniconda.html
+    set -l CONDA_ROOT $HOME/miniconda3
+    if [ -d $CONDA_ROOT ]
+        set -x PATH $HOME/miniconda3/bin $PATH
+        source $CONDA_ROOT/etc/fish/conf.d/conda.fish
+    end
 
-# Init pyenv
-if begin; status --is-interactive; and which pyenv > /dev/null; end
-    source (pyenv init - | psub)
+    set -gx EDITOR (which vim)
+    set -gx PAGER (which less)
 end
-
-
-set -x EDITOR (which vim)
-set -x PAGER (which less)
