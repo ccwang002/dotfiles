3705ff8fa96cffdae58f8950b67aad322efdc005
Liang-Bo Wang <me@liang2.tw>
2017-05-08 16:16:41 -0500

    Use separate settings for Linux and OSX

--- omf/init.fish
+++ omf/init.fish
@@ -3,24 +3,39 @@ if status --is-login
     set -gx LANG en_US.UTF-8
     set -gx LC_CTYPE en_US.UTF-8
 
-    # Update PATH for homebrew
-    test -d "$HOME/.cabal/bin"; and set -x PATH $HOME/.cabal/bin $PATH
-    set -x PATH /usr/local/sbin /usr/local/bin $PATH
+    switch (uname)
+        case 'Darwin*'
+            # Settings on MacOS
+            # Update PATH for homebrew
+            test -d "$HOME/.cabal/bin"; and set -x PATH $HOME/.cabal/bin $PATH
+            set -x PATH /usr/local/sbin /usr/local/bin $PATH
 
-    # For rustup.rs
-    # Ref: https://www.rustup.rs/
-    test -d "$HOME/.cargo/bin"; and set -x PATH $HOME/.cargo/bin $PATH
+            # For rustup.rs
+            # Ref: https://www.rustup.rs/
+            test -d "$HOME/.cargo/bin"; and set -x PATH $HOME/.cargo/bin $PATH
+
+            # conda http://conda.pydata.org/miniconda.html
+            set -l CONDA_ROOT $HOME/miniconda3
+            if [ -d $CONDA_ROOT ]
+                set -x PATH $HOME/miniconda3/bin $PATH
+                source $CONDA_ROOT/etc/fish/conf.d/conda.fish
+            end
+
+        case 'Linux*'
+            # Settings on Linux (mostly servers)
+            # Rust environment by rustup.rs
+            if [ -n $CARGO_HOME -a $RUSTUP_HOME ]
+                set -x PATH $CARGO_HOME/bin $PATH
+                set -x CARGO_HOME $CARGO_HOME
+                set -x RUSTUP_HOME $RUSTUP_HOME
+            end
+        case '*'
+            echo "Unrecognized OS: (uname -a)!"
+    end
 
     # Python environment
     # Disable Python venv prompt changing
     set -gx VIRTUAL_ENV_DISABLE_PROMPT "yes"
-
-    # conda http://conda.pydata.org/miniconda.html
-    set -l CONDA_ROOT $HOME/miniconda3
-    if [ -d $CONDA_ROOT ]
-        set -x PATH $HOME/miniconda3/bin $PATH
-        source $CONDA_ROOT/etc/fish/conf.d/conda.fish
-    end
 
     set -gx EDITOR (which vim)
     set -gx PAGER (which less)
