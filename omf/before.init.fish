d2f521c17fc1b70a891b011d596351e2d11ccb14
Liang-Bo Wang <me@liang2.tw>
2017-05-18 13:35:09 -0500

    Abbreviate path in title

--- omf/before.init.fish
+++ omf/before.init.fish
@@ -29,4 +29,4 @@ set -g theme_show_exit_status yes
 set -g theme_title_display_path yes
 set -g theme_title_display_process yes
 set -g theme_title_display_user yes
-set -g theme_title_use_abbreviated_path no
+set -g theme_title_use_abbreviated_path yes
