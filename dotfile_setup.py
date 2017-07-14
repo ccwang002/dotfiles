7bc3eced95cf324e7fb89858194af3e09088ecbc
Liang-Bo Wang <me@liang2.tw>
2017-07-14 15:06:29 -0500

    Make linking optional

--- dotfile_setup.py
+++ dotfile_setup.py
@@ -11,6 +11,8 @@ Destination         Source under $PWD
 
 Unexisted folder will be created (but may fail).
 """
+import argparse
+from fnmatch import fnmatch
 from pathlib import Path
 
 symlink_settings = '''\
@@ -34,9 +36,14 @@ symlink_settings = '''\
 '''
 
 
-def main():
+def main(patterns_to_link):
     for setting_line in symlink_settings.splitlines():
         dest_pth, src_pth = setting_line.rstrip().split(maxsplit=1)
+        # Check if user wants to link this pattern
+        if patterns_to_link and \
+                not any(fnmatch(dest_pth, pttn) for pttn in patterns_to_link):
+            print('Skip linking', dest_pth)
+            continue
         try:
             # For Python 3.5+
             dest = Path(dest_pth).expanduser()
@@ -44,7 +51,7 @@ def main():
             import os.path as op
             dest = Path(op.expanduser(dest_pth))
         src = Path(src_pth).resolve()
-        print('Linking', dest, '->', src)
+        print('Link', dest, '->', src)
         if dest.exists():
             print('... symlink existed ->', dest.resolve(), '(removed)')
             dest.unlink()
@@ -53,4 +60,11 @@ def main():
         dest.symlink_to(src)
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+        description="Link dotfiles under user's home "
+                    "(controlled patterns to link by --only)")
+    parser.add_argument(
+        '--only', action='append',
+        help='Only link the files that match the shell-like pattern')
+    args = parser.parse_args()
+    main(patterns_to_link=args.only)
