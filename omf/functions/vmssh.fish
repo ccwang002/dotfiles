6d335dd79dc450d96eaa94998dbef1f9dd2f052b
Liang-Bo Wang <me@liang2.tw>
2018-02-08 11:03:43 -0600

    Stricter vm ssh settings

--- omf/functions/vmssh.fish
+++ omf/functions/vmssh.fish
@@ -1,3 +1,3 @@
 function vmssh --description 'use ssh without key nor host check'
-    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $argv
+    ssh -o UserKnownHostsFile=/tmp/vm_known_hosts $argv
 end
