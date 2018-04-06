2075555553614ca5be524e892258783f75c3cb89
Liang-Bo Wang <me@liang2.tw>
2018-04-06 11:11:38 -0500

    Remove virtualfish

--- omf/pkg/virtualfish/init.fish
+++
@@ -1,5 +0,0 @@
-function init -a path --on-event init_virtualfish
-    source $path/functions/virtual.load
-    source $path/functions/auto_activation.load
-    source $path/functions/global_requirements.load
-end
