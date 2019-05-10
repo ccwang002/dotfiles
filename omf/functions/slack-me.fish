75c86e9907d46ae9d4d30df6b2388a767a220de7
Liang-Bo Wang <liang-bo.wang@wustl.edu>
2019-05-10 10:21:10 -0500

    Fix slack-me message

--- omf/functions/slack-me.fish
+++ omf/functions/slack-me.fish
@@ -1,5 +1,5 @@
 function slack-me --wraps 'slack-cli' --description "Slack myself a job's ended"
     # Capture the previous job status
     set -l prev_status $status
-    slack-cli --team mgidinglab -d bobo "A job on "(hostname)" has ended (exit status $prev_status): $msg"
+    slack-cli --team mgidinglab -d bobo "A job on "(hostname)" has ended (exit status $prev_status): $argv"
 end
