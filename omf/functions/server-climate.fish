42281e5af33c654db2cc6ceea556d6844bea6967
Liang-Bo Wang <liang-bo.wang@wustl.edu>
2019-05-28 09:40:32 -0500

    Add new katmai servers to monitor

--- omf/functions/server-climate.fish
+++ omf/functions/server-climate.fish
@@ -1,12 +1,12 @@
 function server-climate --description "Check the server CPU usage."
     switch (hostname)
         case 'katmai'
-            parallel -j4 --keep-order \
+            parallel -j4 \
                 'ssh {} "echo \$(hostname)\":\"' \
                 '\$(top -b -n2 -d 1 -p 1 ' \
                 '| sed -nr \"s/.*, *([0-9.]+)* id.*/\1/p\"' \
                 '| awk \'{print 100 - \$1\"%\"}\')"' \
-                ::: katmai kenai kobuk sitka  # add more server names here
+                ::: katmai kenai kobuk sitka acadia congaree guadalupe tortugas  # add more server names here
         case '*'
             printf "I don't know how to read the CPU usage here. Try htop instead."
     end
