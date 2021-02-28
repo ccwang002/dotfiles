86f0ef005ea65fdd3e62fc437d5e189fc24fb3fa
Liang-Bo Wang <liang-bo.wang@wustl.edu>
2021-02-28 13:15:34 -0600

    Implement a new Slack bot

--- omf/functions/slack-me.fish
+++ omf/functions/slack-me.fish
@@ -1,5 +1,19 @@
-function slack-me --wraps 'slack-cli' --description "Slack myself a job's ended"
+function slack-me --description "Slack myself a job's ended"
     # Capture the previous job status
     set -l prev_status $status
-    slack-cli --team mgidinglab -d bobo "A job on "(hostname)" has ended (exit status $prev_status): $argv"
+
+    if not set -q SLACK_BOT_TOKEN; or not set -q SLACK_BOT_CHANNEL
+        echo -s "Error: Slack bot integration is not set up. " \
+            "Configure the shell variables below:"\n \
+            "  set -U SLACK_BOT_TOKEN 'xoxb-....'"\n \
+            "  set -U SLACK_BOT_CHANNEL 'channel_id'" \
+            1>&2
+        return 1
+    end
+
+    curl \
+        -F token="$SLACK_BOT_TOKEN" \
+        -F channel="$SLACK_BOT_CHANNEL" \
+        -F text="A job on "(hostname)" has ended (exit $prev_status): $argv" \
+        https://slack.com/api/chat.postMessage
 end
